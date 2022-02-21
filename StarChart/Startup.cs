120f28c7d9b6eefb573fc4ae85541abea8db3314
oscar velasco <j.oscar.velasco@gmail.com>
2022-02-21 13:55:31 -0500

    Update Startup.cs
    updates to Startup

--- StarChart/Startup.cs
+++ StarChart/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using StarChart.Data;
 
 namespace StarChart
 {
@@ -10,13 +12,18 @@ namespace StarChart
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            
+            //call AddMvc on services object
+            services.AddMvc();
+            //call AddDbContext on services with arguments options => options.UseInMemoryDatabase("StarChart")
+            //points to apps DbContext
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("StarChart"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            
+            //call to use Mvc framework
+            app.UseMvc();
         }
     }
 }
