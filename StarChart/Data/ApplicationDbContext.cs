21b2f0cfe328555563d7dfff4bb50fcff222b54f
oscar velasco <j.oscar.velasco@gmail.com>
2022-02-21 14:05:40 -0500

    added CelestialObject and bind it to app db context
    added CelestialObject and bind it to app db context

--- StarChart/Data/ApplicationDbContext.cs
+++ StarChart/Data/ApplicationDbContext.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using StarChart.Models;
 
 namespace StarChart.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        //add CelestialObjects to app db context
+        public DbSet<CelestialObject> CelestialObjects { get; set; }
+
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
         }
