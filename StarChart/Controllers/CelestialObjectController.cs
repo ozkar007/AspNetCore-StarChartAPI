887d98558d1190bfede7712ae6b66aa11178cfa5
Oscar-Velasco_ivanti <Oscar.Velasco@ivanti.com>
2022-03-22 16:25:27 -0400

    completed last module: Create CelestialObjectControllers's Post, Put, Patch, and Delete actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StarChart.Data;
 using StarChart.Models;
 
@@ -22,6 +23,95 @@ namespace StarChart.Controllers
         public CelestialObjectController(ApplicationDbContext context)
         {
             _context = context;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id) 
+        {
+            //locate ALL CelestialObjects that have the same id/OrbitObjectId to delete
+            
+            if (_context.CelestialObjects.FirstOrDefault(x => x.Id == id) == null )
+            {
+                return NotFound();
+            }
+
+            //remove sequence of records
+            var celObjects = _context.CelestialObjects.ToList().Where(x => x.Id == id || x.OrbitedObjectId == id);
+            _context.RemoveRange(celObjects);
+            _context.SaveChanges();
+
+            return NoContent();
+
+        }
+
+        [HttpPatch("{id}/{name}")]
+        public IActionResult RenameObject(int id, string name) 
+        {
+            //locate CelestialObject based on id
+            var patchCelObj = _context.CelestialObjects.FirstOrDefault(x => x.Id == id);
+            if (patchCelObj == null)
+            {
+                return NotFound();
+            }
+
+            //patch patchCelObj
+            patchCelObj.Name = name;
+            _context.CelestialObjects.Update(patchCelObj);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, CelestialObject celestialObject) 
+        {
+            //find CelestialObject based on param Id
+            //can we reuse by calling GetById(id),
+            //not sure how to work with IActionResult, to access CelestialObject
+            var updateCelObj = _context.CelestialObjects.FirstOrDefault(c => c.Id == id);
+            if (updateCelObj == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                //update found CelestialObject with data
+                //from param CelestialObject
+                updateCelObj.Name = celestialObject.Name;
+                updateCelObj.OrbitalPeriod = celestialObject.OrbitalPeriod;
+                updateCelObj.OrbitedObjectId = celestialObject.OrbitedObjectId;
+
+                _context.CelestialObjects.Update(updateCelObj);
+                _context.SaveChanges();
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody]CelestialObject payload) 
+        {
+            //happy path:
+            //we assume CelestialObject payload has all properties set
+            //alternative option wrap in try/catch block
+            _context.CelestialObjects.AddAsync(payload);//adds entity DbSet in memory
+            _context.SaveChangesAsync();//saves entity to db
+
+            var cObject = _context.CelestialObjects.Find(1);
+            
+            return CreatedAtRoute("GetById", new { cObject.Id }, cObject);
+
+            /*
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw new InvalidOperationException();
+            }
+            */
         }
 
         [HttpGet("{id:int}", Name = "GetById")]
